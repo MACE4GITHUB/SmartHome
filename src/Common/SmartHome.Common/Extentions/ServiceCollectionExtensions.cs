1e7b45e166a3aeff7c3d796297696ab6a9ed58fc
MACE <d.mace@live.com>
2021-04-24 12:47:21 +0300

    Add Decorator
    AddDecorator, TryAddDecorator [one by one]

---
+++ src/Common/SmartHome.Common/Extentions/ServiceCollectionExtensions.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+using System.Linq;
+
+namespace SmartHome.Common.Extentions
+{
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Tries add Decorator.
+        /// </summary>
+        /// <typeparam name="TInterface"></typeparam>
+        /// <typeparam name="TDecorator"></typeparam>
+        /// <param name="serviceCollection"></param>
+        /// <param name="configureDecorateServices"></param>
+        public static void TryAddDecorator<TInterface, TDecorator>
+            (this IServiceCollection serviceCollection, Action<IServiceCollection> configureDecorateServices)
+            where TInterface : class
+            where TDecorator : class, TInterface
+        {
+            if (!serviceCollection.Where(sd => sd.ServiceType == typeof(TInterface) 
+                                               && sd.GetImplementationType() == typeof(TDecorator)).ToArray().Any())
+            {
+                serviceCollection.AddDecorator<TInterface, TDecorator>(configureDecorateServices);
+            }
+        }
+
+        /// <summary>
+        /// Adds Decorator.
+        /// </summary>
+        /// <typeparam name="TInterface"></typeparam>
+        /// <typeparam name="TDecorator"></typeparam>
+        /// <param name="serviceCollection"></param>
+        /// <param name="configureDecorateServices"></param>
+        public static void AddDecorator<TInterface, TDecorator>
+        (this IServiceCollection serviceCollection, Action<IServiceCollection> configureDecorateServices)
+            where TInterface : class
+            where TDecorator : class, TInterface
+        {
+            var decorateServices = new ServiceCollection();
+            configureDecorateServices(decorateServices);
+
+            var decorateDescriptor = decorateServices.SingleOrDefault(sd => sd.ServiceType == typeof(TInterface));
+
+            if (decorateDescriptor == null)
+            {
+                throw new InvalidOperationException($"{typeof(TInterface).Name} is not configured.");
+            }
+
+            decorateServices.Remove(decorateDescriptor);
+            serviceCollection.Add(decorateServices);
+
+            var decoratorInstanceFactory = ActivatorUtilities.CreateFactory(
+                typeof(TDecorator), new[] { typeof(TInterface) });
+
+            var decorateImplType = decorateDescriptor.GetImplementationType();
+
+            Func<IServiceProvider, TDecorator> decoratorFactory = sp =>
+            {
+                var decorate = sp.GetRequiredService(decorateImplType);
+                var decorator = (TDecorator)decoratorInstanceFactory(sp, new[] { decorate });
+                return decorator;
+            };
+
+            var decoratorDescriptor = ServiceDescriptor.Describe(typeof(TInterface), decoratorFactory, decorateDescriptor.Lifetime);
+
+            decorateDescriptor = UpdateDecorateDescriptor(decorateDescriptor);
+
+            serviceCollection.Add(decorateDescriptor);
+            serviceCollection.Add(decoratorDescriptor);
+        }
+
+        private static ServiceDescriptor UpdateDecorateDescriptor(ServiceDescriptor decorateDescriptor)
+        {
+            var decorateImplType = decorateDescriptor.GetImplementationType();
+
+            decorateDescriptor = decorateDescriptor.ImplementationFactory != null
+                ? ServiceDescriptor.Describe(decorateImplType, decorateDescriptor.ImplementationFactory, decorateDescriptor.Lifetime)
+                : decorateDescriptor.ImplementationInstance != null
+                    ? ServiceDescriptor.Singleton(decorateImplType, decorateDescriptor.ImplementationInstance)
+                    : ServiceDescriptor.Describe(decorateImplType, decorateImplType, decorateDescriptor.Lifetime);
+
+            return decorateDescriptor;
+        }
+
+        private static Type GetImplementationType(this ServiceDescriptor serviceDescriptor) =>
+            serviceDescriptor.ImplementationType ?? (serviceDescriptor.ImplementationInstance != null
+                ? serviceDescriptor.ImplementationInstance.GetType()
+                : serviceDescriptor.ImplementationFactory != null
+                    ? serviceDescriptor.ImplementationFactory.GetType().GenericTypeArguments[1]
+                    : throw new InvalidOperationException($"The implementation type of {serviceDescriptor.GetType().Name} is not registered."));
+    }
+}
---
+++ src/Common/SmartHome.CommonTests/Extentions/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,135 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace SmartHome.Common.Extentions.Tests
+{
+    public class ServiceCollectionExtensionsTests
+    {
+        [Fact]
+        public void AddDecoratorTest()
+        {
+
+            var provider = ConfigureProvider(s =>
+            {
+                s.AddTransient<IService, SomeRandomService>();
+                s.AddDecorator<IDecoratedService, Decorator>(c =>
+                {
+                    c.AddSingleton<IDecoratedService, Decorated>();
+                });
+                s.AddDecorator<IDecoratedService, Decorator>(c =>
+                {
+                    c.AddSingleton<IDecoratedService, Decorated>();
+                });
+            });
+
+            var count = provider.GetServices<IDecoratedService>().ToList().Count;
+            count.Should().Be(2);
+
+            var instance = provider.GetRequiredService<IDecoratedService>();
+
+            var decorator = Assert.IsType<Decorator>(instance);
+
+            Assert.IsType<Decorated>(decorator.Inner);
+        }
+
+        [Fact]
+        public void TryAddDecoratorSomeTest()
+        {
+            var provider = ConfigureProvider(s =>
+            {
+                s.TryAddDecorator<IDecoratedService, Decorator>(sc1 =>
+                {
+                    sc1.AddTransient<IService, SomeRandomService>();
+                    sc1.AddDecorator<IDecoratedService, OtherDecorated>(
+                        sc2 =>
+                        {
+                            sc2.AddSingleton<IDecoratedService, Decorated>();
+                        });
+                });
+            });
+
+            var count = provider.GetServices<IDecoratedService>().ToList().Count;
+            count.Should().Be(1);
+
+            var instance = provider.GetRequiredService<IDecoratedService>();
+
+            var decorator = Assert.IsType<Decorator>(instance);
+
+            Assert.IsType<OtherDecorated>(decorator.Inner);
+        }
+
+        [Fact]
+        public void TryAddDecoratorTest()
+        {
+
+            var provider = ConfigureProvider(s =>
+            {
+                s.AddDecorator<IDecoratedService, Decorator>(c =>
+                {
+                    c.AddSingleton<IDecoratedService, Decorated>();
+                });
+                s.TryAddDecorator<IDecoratedService, Decorator>(c =>
+                {
+                    c.AddSingleton<IDecoratedService, Decorated>();
+                });
+            });
+
+            var count = provider.GetServices<IDecoratedService>().ToList().Count;
+            count.Should().Be(1);
+
+            var instance = provider.GetRequiredService<IDecoratedService>();
+
+            var decorator = Assert.IsType<Decorator>(instance);
+
+            Assert.IsType<Decorated>(decorator.Inner);
+        }
+
+        private static ServiceProvider ConfigureProvider(Action<IServiceCollection> configure)
+        {
+            var services = new ServiceCollection();
+
+            configure(services);
+
+            return services.BuildServiceProvider();
+        }
+
+        public interface IDecoratedService { }
+
+        public interface IService { }
+
+        public class Decorated : IDecoratedService
+        {
+            public Decorated(IService injectedService = null)
+            {
+                InjectedService = injectedService;
+            }
+
+            public IService InjectedService { get; }
+        }
+
+        public class Decorator : IDecoratedService
+        {
+            public Decorator(IDecoratedService inner, IService injectedService = null)
+            {
+                Inner = inner;
+                InjectedService = injectedService;
+            }
+
+            public IDecoratedService Inner { get; }
+
+            public IService InjectedService { get; }
+        }
+
+        public class OtherDecorated : IDecoratedService
+        {
+            public OtherDecorated(IDecoratedService inner = null, IService injectedService = null)
+            {
+            }
+        }
+
+        private class SomeRandomService : IService { }
+    }
+}
